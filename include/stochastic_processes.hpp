21f8d8eb1d15c46075b26d6a9e4c722eb8c6de24
owlas <ollielaslett@icloud.com>
2017-01-03 18:53:19 +0000

    add function to downsample wiener paths

--- include/stochastic_processes.hpp
+++ include/stochastic_processes.hpp
@@ -7,15 +7,28 @@
 #define STOC_H
 #include <cstdlib>
 
+namespace stochastic
+{
 // Computes the ito correction term for a stochastic process from its
 // diffusion jacobian. Used to convert between Stratonovich and Ito
 // drift derivatives.
-void strato_to_ito( double *ito_drift, const double *strato_drift,
-                    const double *diffusion, const double *diffusion_jacobian,
-                    const size_t n_dims, const size_t wiener_dims );
+    void strato_to_ito( double *ito_drift, const double *strato_drift,
+                        const double *diffusion, const double *diffusion_jacobian,
+                        const size_t n_dims, const size_t wiener_dims );
 
-void ito_to_strato( double *strato_drift, const double *ito_drift,
-                    const double *diffusion, const double *diffusion_jacobian,
-                    const size_t n_dims, const size_t wiener_dims );
+    void ito_to_strato( double *strato_drift, const double *ito_drift,
+                        const double *diffusion, const double *diffusion_jacobian,
+                        const size_t n_dims, const size_t wiener_dims );
+
+
+    /*
+      Takes a wiener path [w1 w2 w3 w4 ...] and reduces to a coarser path.
+      e.g. reduction factor of two gives [w1+w2 w3+w4 w5+w6 ...]
+
+      Returns the final size of the array
+    */
+    size_t downsample_wiener_path(
+        double *increments, size_t length, size_t reduction_factor );
+}
 
 #endif
--- lib/stochastic_processes.cpp
+++ lib/stochastic_processes.cpp
@@ -1,8 +1,9 @@
 #include "../include/stochastic_processes.hpp"
 
-void strato_to_ito( double *ito_drift, const double *strato_drift,
-                    const double *diffusion, const double *diffusion_jacobian,
-                    const size_t n_dims, const size_t wiener_dims )
+void stochastic::strato_to_ito(
+    double *ito_drift, const double *strato_drift,
+    const double *diffusion, const double *diffusion_jacobian,
+    const size_t n_dims, const size_t wiener_dims )
 {
     for( unsigned int i=0; i<n_dims; i++ )
         for( unsigned int j=0; j<wiener_dims; j++ )
@@ -13,9 +14,10 @@ void strato_to_ito( double *ito_drift, const double *strato_drift,
                     * diffusion_jacobian[i*n_dims*wiener_dims+j*n_dims+k];
 }
 
-void ito_to_strato( double *strato_drift, const double *ito_drift,
-                    const double *diffusion, const double *diffusion_jacobian,
-                    const size_t n_dims, const size_t wiener_dims )
+void stochastic::ito_to_strato(
+    double *strato_drift, const double *ito_drift,
+    const double *diffusion, const double *diffusion_jacobian,
+    const size_t n_dims, const size_t wiener_dims )
 {
     for( unsigned int i=0; i<n_dims; i++ )
         for( unsigned int j=0; j<wiener_dims; j++ )
@@ -24,4 +26,18 @@ void ito_to_strato( double *strato_drift, const double *ito_drift,
                     - 0.5
                     * diffusion[k*wiener_dims+j]
                     * diffusion_jacobian[i*n_dims*wiener_dims+j*n_dims+k];
+}
+
+size_t stochastic::downsample_wiener_path( double *inc, size_t N, size_t R )
+{
+    // If no reduction factor required then return the original increments
+    if( R==0 )
+        return N;
+
+    for( unsigned int i=0; i<N; i++ )
+        if( i%R )
+            inc[i/R] += inc[i];
+        else
+            inc[i/R] = inc[i];
+    return N/R;
 }
