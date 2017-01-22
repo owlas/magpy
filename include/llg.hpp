60cbf4d13f97ef6c7b5a5373436e69199ac79779
owlas <ollielaslett@icloud.com>
2017-01-22 12:13:24 +0000

    add llg joint update functions

--- include/llg.hpp
+++ include/llg.hpp
@@ -18,5 +18,42 @@ namespace llg
     void diffusion_jacobian( double *jacobian, const double *state,
                              const double time, const double sr,
                              const double alpha );
+
+    /*
+      Computes the effective field, llg drift, and llg diffusion
+      Non-const are outputs, const are inputs.
+
+      Assumes single particle with uniaxial anisotropy
+    */
+    void sde_with_update( double *drift,
+                          double *diffusion,
+                          double *heff,
+                          const double *current_state,
+                          const double drift_time,
+                          const double diffusion_time,
+                          const double *happ,
+                          const double *anisotropy_axis,
+                          const double alpha,
+                          const double noise_power );
+
+    /*
+      Computes the effective field, llg drift, llg diffusion and their
+      respective derivatives.
+
+      Assumes a single particle with uniaxial anisotropy
+    */
+    void jacobians_with_update( double *drift,
+                                double *diffusion,
+                                double *drift_jac,
+                                double *diffusion_jac,
+                                double *heff,
+                                double *heff_jac,
+                                const double *current_state,
+                                const double drift_time,
+                                const double diffusion_time,
+                                const double *happ,
+                                const double *anisotropy_axis,
+                                const double alpha,
+                                const double noise_power );
 }
 #endif
