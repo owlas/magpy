a9f64d90fa6069c4ae3e7efcc52a213a56c032ae
Oliver Laslett <o.laslett@soton.ac.uk>
2017-06-01 14:46:39 +0100

    add dipolar field and change field term interface

--- include/field.hpp
+++ include/field.hpp
@@ -6,10 +6,11 @@
 #ifndef FIELD_H
 #define FIELD_H
 #include <cstddef>
+#include <functional>
 
 namespace field
 {
-    // constant field term
+    /// Constant field term
     double constant( const double h, const double t);
 
     // field strength h, frequency f, at time t
@@ -20,18 +21,60 @@ namespace field
     double square_fourier( const double h, const double f,
                            const size_t n_compononents, double t );
 
+    void multi_add_applied_Z_field_function(
+        double *heff,
+        const std::function<double(const double)> &hfunc,
+        const double t,
+        const size_t N );
+
     // Computes the anisotropy field
     void uniaxial_anisotropy( double *h_anis, const double *magnetisation,
                               const double *anis_axis );
 
-    /*
-      Compute the Jacobian of the uniaxial anisotropy field with respect
-      to the magnetisation.
-      >> J = ∂h(m)/∂m
-      axis: IN is the 3 dimensional anisotropy axis
-      jac: OUT is the 3x3 dimensional Jacobian
-    */
-    void uniaxial_anisotropy_jacobian( double *jac, const double *axis );
+    // Computes the anisotropy field for multiple particles
+    void multi_add_uniaxial_anisotropy(
+        double *h,
+        const double *states,
+        const double *axes,
+        const double *k_reduced,
+        const size_t N );
+
+    void uniaxial_anisotropy_jacobian(
+        double *jac,
+        const double *axis );
+
+    void multi_uniaxial_anisotropy_jacobian(
+        double *jac,
+        const double *axes,
+        const double *anis,
+        const size_t N );
+
+
+    void dipolar(
+        double *field,
+        const double ms,
+        const double k_av,
+        const double *v_reduced,
+        const double *mag,
+        const double *dists,
+        const double *dist_cubes,
+        const size_t N );
+
+    double dipolar_prefactor(
+        const double ms,
+        const double k_av );
+
+    void dipolar_add_p2p_term(
+        double *out,
+        const double vj,
+        const double rij3,
+        const double *mj,
+        const double *dist,
+        const double prefactor );
+
+    void zero_all_field_terms(
+        double *h,
+        const size_t N);
 
 }
 #endif
--- lib/field.cpp
+++ lib/field.cpp
@@ -7,6 +7,7 @@
  * @date 2017
  */
 #include "../include/field.hpp"
+#include "../include/constants.hpp"
 #define _USE_MATH_DEFINES
 #include<cmath>
 
@@ -74,6 +75,16 @@ double field::square_fourier( const double h,
     return field;
 }
 
+/// Add the applied field in the z direction
+void field::multi_add_applied_Z_field_function(
+    double *heff, const std::function<double(const double)> &hfunc,
+    const double t, const size_t N )
+{
+    double happ = hfunc( t );
+    for( unsigned int i=0; i<N; i++ )
+        heff[i*3+2] += happ;
+}
+
 /// Effective field contribution from uniaxial anisotropy
 /**
  * The effective field experienced by a single particle with a
@@ -92,12 +103,38 @@ void field::uniaxial_anisotropy(
     h_anis[2] = dot*axis[2];
 }
 
+/// Add the uniaxial anisotropy term term to the field of N particles
+/**
+ * @param[out] h effective field incremented with the anisotropy term
+ * @param[in] states magnetic state of the particles (length 3N)
+ * @param[in] axes anisotropy axis of each particle (length 3N)
+ * @param[in] k_reduced the reduced anisotropy constant for each particle (length N)
+ * @param[in] N number of particles
+ */
+void field::multi_add_uniaxial_anisotropy(
+    double *h, const double *states,
+    const double *axes, const double *k_reduced, const size_t N )
+{
+    for( unsigned int n=0; n<N; n++ )
+    {
+        double k_red = k_reduced[n];
+        const double *axis = axes+3*n;
+        const double *state = states+3*n;
+
+        double dot = state[0]*axis[0]+state[1]*axis[1]+state[2]*axis[2];
+        dot *= k_red;
+        h[3*n+0] += dot*axis[0];
+        h[3*n+1] += dot*axis[1];
+        h[3*n+2] += dot*axis[2];
+    }
+}
+
 /// Jacobian of the uniaxial anisotropy effective field term
 /**
  * The Jacobian of a particle's uniaxial anisotropy with respect to
  * it's magnetisation value.
  * \f$J_h(m) = \frac{\partial h(m)}{\partial m}\f$
- * @param[out] jac the jacobian of the effective field [length 3*3]
+ * @param[out] jac the jacobian of the effective field [length 3x3]
  * @param[in]  axis the anisotropy axis of the particle [length 3]
  */
 void field::uniaxial_anisotropy_jacobian(
@@ -106,4 +143,88 @@ void field::uniaxial_anisotropy_jacobian(
     for( unsigned int i=0; i<3; i++ )
         for( unsigned int j=0; j<3; j++ )
             jac[i*3+j] = axis[i]*axis[j];
+}
+
+/// Jacobian of the uniaxial anisotropy effective field term for many particles
+/**
+ * The Jacobian of a particle's uniaxial anisotropy with respect to
+ * it's magnetisation value.
+ * \f$J_h(m) = \frac{\partial h(m)}{\partial m}\f$
+ * @param[out] jac the jacobian of the effective field [length 3Nx3N]
+ * @param[in]  axis the anisotropy axis of the particle [length 3N]
+ * @param[in] N number of particles
+ */
+void multi_add_uniaxial_anisotropy_jacobian( double *jac, const double *axes,
+                                             double *k_reduced, size_t N )
+{
+    for( unsigned int i=0; i<N; i++ )
+    {
+        // place on diagonal
+        for( unsigned int x=0; x<3; x++ )
+            for( unsigned int y=0; y<3; y++ )
+                jac[(3*i+x)*3*N + 3*i+y] += k_reduced[i] * axes[3*i+x] * axes[3*i+y];
+    }
+}
+
+/**
+ * field is Nx3 and is the effective field on each particle
+ * ms is the same for all particles
+ * k_av is the average anisotropy constant for all particles
+ * v_reduced is the reduced volume of each particle
+ * v_av is the average volume for all particles
+ * mag is Nx3 long and is the magnetic state of each of the N particles
+ * dists is NxNx3 long and is the distance between each pair of particles
+ * N is the number of particles
+ * dist_cubes is NxN and is the reduced cubed distance between each pair
+ */
+void field::dipolar(double *field, const double ms, const double k_av,
+                    const double *v_reduced, const double *mag,
+                    const double *dists, const double *dist_cubes,
+                    const size_t N  )
+{
+    double prefactor = field::dipolar_prefactor( ms, k_av );
+    // Now compute the ting
+    for( unsigned int i=0; i<N; i++ )
+    {
+        double *particle_field = field + (3*i);
+        particle_field[0] = particle_field[1] = particle_field[2] = 0;
+
+        for( unsigned int j=0; j<i; j++ )
+             field::dipolar_add_p2p_term( particle_field, v_reduced[j], dist_cubes[i*N + j],
+                                         mag + j*3, dists + i*N*3 + j*3, prefactor );
+    }
+
+}
+
+/// Prefactor term (helper for field::dipolar)
+/**
+ * prefactor is \f$\frac{\mu0 M_s^2}{8\pi\bar{K}}\f$
+ * @param[in] ms saturation magnetisation
+ * @param[in] k_av average anisotropy constant for system
+ * @returns prefactor term
+ */
+double field::dipolar_prefactor( const double ms, const double k_av )
+{
+    return constants::MU0 * ms * ms / 8.0 / M_PI / k_av;
+}
+
+void field::dipolar_add_p2p_term( double *out, const double vj, const double rij3,
+                                  const double *mj, const double *dist,
+                                  const double prefactor )
+{
+    double term_dotprod = mj[0]*dist[0] + mj[1]*dist[1] + mj[2]*dist[2];
+    double term_1 = vj / rij3;
+    for( unsigned int i=0; i<3; i++ )
+        out[i] += prefactor * term_1 * ( 3 * term_dotprod * dist[i] - mj[i] );
+}
+
+/// Set all N values of the field to zero
+/**
+ * @param[out] h pointer to array of N doubles will all be set to zero
+ * @param[in] N number of elements in array
+ */
+void field::zero_all_field_terms( double *h, const size_t N)
+{
+    for( unsigned int i=0; i<N; i++ )
+        h[i] = 0.0;
 }
