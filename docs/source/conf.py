b8827a9020e54afeecf70a0e200c69ff840dfc2f
owlas <ollielaslett@icloud.com>
2017-10-10 22:58:03 +0100

    mock external packages in doc building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,9 @@ extensions = [
     'sphinx.ext.napoleon'
 ]
 
+# Mock imports that we don't need
+autodoc_mock_imports = ['matplotlib', 'toolz', 'numpy', 'scipy', 'joblib']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
