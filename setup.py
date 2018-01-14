43bf71b0c45ee1e1cd568dec9fb43bf620123f9c
owlas <ollielaslett@icloud.com>
2018-01-14 22:44:38 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ gnu_libs = ['moma']
 
 setup(
     name='magpy',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     ext_modules= cythonize(Extension(
         name='magpy.core',
