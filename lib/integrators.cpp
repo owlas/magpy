809cf8734defb37d244228b531effb7655c6e8e1
owlas <ollielaslett@icloud.com>
2018-02-07 09:14:05 +0000

    use large k in implicit integrator

--- lib/integrators.cpp
+++ lib/integrators.cpp
@@ -596,7 +596,8 @@ int integrator::implicit_midpoint(
 {
     // Implicit method depends on a modified random variable
     // See http://epubs.siam.org/doi/pdf/10.1137/S0036142901395588 for details
-    double Ah = std::sqrt( 2*std::abs( std::log( dt ) ) );
+    double k = 1000;
+    double Ah = std::sqrt( 2*k*std::abs( std::log( dt ) ) );
     for( unsigned int i=0; i<w_dim; i++ )
         dwm[i] = std::max( -Ah, std::min( Ah, dw[i] ) ) * std::sqrt( dt );
 
